df65e939dc8fad1286fd5f76f85772ef5effd566
Sean Nemann <seannemann@Beams-MacBook-Pro.local>
2019-08-21 20:20:11 -0400

    Removed api controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'api/request_token'
   get '/auth/:provider/callback', to: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
