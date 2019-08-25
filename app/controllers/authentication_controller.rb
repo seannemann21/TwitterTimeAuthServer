9b12ff3b486fd8d42e40ba42ffe79406af97bdf1
Sean Nemann <seannemann@Beams-MacBook-Pro.local>
2019-08-25 13:02:03 -0400

    Testing param passing to omniauth

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -1,6 +1,7 @@
 class AuthenticationController < ApplicationController
   def callback
     auth_hash = request.env['omniauth.auth']
+    puts request.env["omniauth.params"].to_yaml
     credentials = auth_hash[:credentials]
     redirect_to "exp://10.0.10.214:19000?token=#{credentials[:token]}&secret=#{credentials[:secret]}"
   end
