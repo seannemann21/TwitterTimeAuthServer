5f0b54d14cfa92552f549b53642458a2bcdfbe3c
Sean Nemann <seannemann@Beams-MacBook-Pro.local>
2019-08-25 13:06:40 -0400

    Redirecting to callback passed through querystring after authentication

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,6 +3,6 @@ class AuthenticationController < ApplicationController
     auth_hash = request.env['omniauth.auth']
     puts request.env["omniauth.params"].to_yaml
     credentials = auth_hash[:credentials]
-    redirect_to "exp://10.0.10.214:19000?token=#{credentials[:token]}&secret=#{credentials[:secret]}"
+    redirect_to "#{request.env['omniauth.params']['callback']}?token=#{credentials[:token]}&secret=#{credentials[:secret]}"
   end
 end
