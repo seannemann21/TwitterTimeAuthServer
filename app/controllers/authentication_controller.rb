648d8108037d430a2a58b6c24e9633eb3c82d371
Sean Nemann <seannemann@Beams-MacBook-Pro.local>
2019-08-25 12:23:25 -0400

    testing without params

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -2,6 +2,6 @@ class AuthenticationController < ApplicationController
   def callback
     auth_hash = request.env['omniauth.auth']
     credentials = auth_hash[:credentials]
-    redirect_to "exp://192.168.1.12:19000?token=#{credentials[:token]}&secret=#{credentials[:secret]}"
+    redirect_to "exp://192.168.1.12:19000"#?token=#{credentials[:token]}&secret=#{credentials[:secret]}"
   end
 end
