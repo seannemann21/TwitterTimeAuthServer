313429b5ddff9fb1fc830cea5916fe89356479f8
Sean Nemann <seannemann@Beams-MBP.home>
2019-09-01 12:17:52 -0400

    converting status id from string to int

--- app/controllers/twitter_controller.rb
+++ app/controllers/twitter_controller.rb
@@ -38,6 +38,6 @@ class TwitterController < ApplicationController
       puts "\n\n\n\n\n"
     end
 
-    render :json => client.status(params[:tweet_id], tweet_mode: 'extended')
+    render :json => client.status(params[:tweet_id].to_i, tweet_mode: 'extended')
   end
 end
