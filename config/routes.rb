747aad2cc57284ccabb1872c0fa3c5b5999310a9
Sean Nemann <seannemann@Beams-MBP.home>
2019-09-01 12:01:10 -0400

    Updating routes config file

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'twitter/my_timeline'
+  get 'twitter/full_text'
   get 'api/test'
   get '/auth/:provider/callback', to: 'authentication#callback'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
